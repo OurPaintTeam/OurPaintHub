1efff5627332db39af8a428bba2ec2a747cf3d3a
Tim <timon.skvor@mail,ru>
2025-10-13 23:46:49 +0300

    Add news page

---
+++ frontend/src/pages/NewsPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainLayout from "../layout/MainLayout";
+
+const NewsPage = () => {
+  return (
+    <MainLayout>
+      <h1>Новости</h1>
+      <p>Здесь будут последние новости проекта.</p>
+    </MainLayout>
+  );
+};
+
+export default NewsPage;
