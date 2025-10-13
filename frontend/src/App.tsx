ab7f2a00ad6faa66c104860a4e0afc041d6337e1
Tim <timon.skvor@mail,ru>
2025-10-13 20:09:55 +0300

    fix app page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,80 +1,20 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StartPage from "./pages/StartPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import InputPage from "./pages/InputPage";
+import AccountPage from "./pages/AccountPage";
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <Router>
-      <nav style={{ marginBottom: "20px" }}>
-        <Link to="/hello">Hello</Link> |{" "}
-        <Link to="/welcome">Welcome</Link> |{" "}
-        <Link to="/send">Send Message</Link>
-      </nav>
-
       <Routes>
-        <Route path="/hello" element={<Hello />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/send" element={<SendMessage />} />
+        <Route path="/" element={<StartPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/input" element={<InputPage />} />
+        <Route path="/account" element={<AccountPage />} />
       </Routes>
     </Router>
   );
-}
+};
 
-function Hello() {
-  const [message, setMessage] = useState<string>("Loading...");
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/hello/")
-      .then((res) => res.json())
-      .then((data: { message: string }) => setMessage(data.message))
-      .catch((err: Error) => setMessage("Error: " + err.message));
-  }, []);
-
-  return <h1>{message}</h1>;
-}
-
-function Welcome() {
-  const [message, setMessage] = useState<string>("Loading...");
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/welcome/")
-      .then((res) => res.json())
-      .then((data: { message: string }) => setMessage(data.message))
-      .catch((err: Error) => setMessage("Error: " + err.message));
-  }, []);
-
-  return <h1>{message}</h1>;
-}
-
-function SendMessage() {
-  const [text, setText] = useState<string>("");
-  const [response, setResponse] = useState<string>("");
-
-  const handleSend = async () => {
-    try {
-      const res = await fetch("http://127.0.0.1:8000/api/send/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ text }),
-      });
-      const data: { message: string } = await res.json();
-      setResponse("Sent: " + data.message);
-    } catch (err: any) {
-      setResponse("Error: " + err.message);
-    }
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Введите текст"
-      />
-      <button onClick={handleSend} style={{ marginLeft: "10px" }}>
-        Отправить
-      </button>
-      <p>{response}</p>
-    </div>
-  );
-}
+export default App;
