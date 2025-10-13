9593c6282b949562a09879d8892f6ba347b462e2
Tim <timon.skvor@mail,ru>
2025-10-13 23:47:00 +0300

    Add download page

---
+++ frontend/src/pages/DownloadPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MainLayout from "../layout/MainLayout";
+
+const DownloadPage = () => (
+  <MainLayout>
+    <h1>Скачать</h1>
+    <p>Скачать версию.</p>
+  </MainLayout>
+);
+
+export default DownloadPage;
