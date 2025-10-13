12bdc7314d761ae01fd2663c21a60c1a0b74f48c
Tim <timon.skvor@mail,ru>
2025-10-13 23:47:10 +0300

    Add documentation page

---
+++ frontend/src/pages/DocumentationPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MainLayout from "../layout/MainLayout";
+
+const DocumentationPage = () => (
+  <MainLayout>
+    <h1>Документация</h1>
+    <p>Раздел документации содержит инструкции и руководства.</p>
+  </MainLayout>
+);
+
+export default DocumentationPage;
