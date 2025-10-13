d94bb886ed7ef550bd69822f69185b8d88e78818
Tim <timon.skvor@mail,ru>
2025-10-13 20:09:20 +0300

    Added account page

---
+++ frontend/src/pages/AccountPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AccountPage: React.FC = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Добро пожаловать в аккаунт!</h1>
+      <p>Вы успешно вошли в систему.</p>
+    </div>
+  );
+};
+
+export default AccountPage;
