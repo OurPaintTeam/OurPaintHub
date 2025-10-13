8df226e51e68737841d6767198e2b93155374e24
Tim <timon.skvor@mail,ru>
2025-10-13 23:48:35 +0300

    Update realization

--- frontend/src/pages/AccountPage.tsx
+++ frontend/src/pages/AccountPage.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import MainLayout from "../layout/MainLayout";
+import "./AccountPage.scss";
 
-const AccountPage: React.FC = () => {
+const AccountPage = () => {
+  const user = {
+    name: "Иван Петров",
+    email: "ivan.petrov@example.com",
+    id: "ID-20394",
+  };
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Добро пожаловать в аккаунт!</h1>
-      <p>Вы успешно вошли в систему.</p>
-    </div>
+    <MainLayout>
+      <div className="account-info">
+        <div className="avatar"></div>
+        <div className="details">
+          <p><strong>Имя:</strong> {user.name}</p>
+          <p><strong>Email:</strong> {user.email}</p>
+          <p><strong>ID:</strong> {user.id}</p>
+        </div>
+      </div>
+    </MainLayout>
   );
 };
 
