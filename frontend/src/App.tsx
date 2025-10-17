9b8b4fcff145ed404202c364356b1ee561453a82
Бычков Евгений <ardrass@mail.ru>
2025-10-18 00:28:08 +0300

    implement new logic: login = account or for unauth account = login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import NewsPage from "./pages/NewsPage";
 import DocumentationPage from "./pages/DocumentationPage";
@@ -8,16 +8,45 @@ import LoginPage from "./pages/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    //chack user in localstorage
+    const userData = localStorage.getItem('user');
+    setIsAuthenticated(!!userData);
+    setIsLoading(false);
+  }, []);
+
+  const updateAuthState = () => {
+    const userData = localStorage.getItem('user');
+    setIsAuthenticated(!!userData);
+  };
+
+  // listening update in local storage
+  useEffect(() => {
+    const handleStorageChange = () => {
+      updateAuthState();
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+    return () => window.removeEventListener('storage', handleStorageChange);
+  }, []);
+
+  if (isLoading) {
+    return <div>Загрузка...</div>;
+  }
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/news" />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/docs" element={<DocumentationPage />} />
-        <Route path="/download" element={<DownloadPage />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/input" element={<LoginPage />} />
-        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/news" element={<NewsPage isAuthenticated={isAuthenticated} />} />
+        <Route path="/docs" element={<DocumentationPage isAuthenticated={isAuthenticated} />} />
+        <Route path="/download" element={<DownloadPage isAuthenticated={isAuthenticated} />} />
+        <Route path="/account" element={isAuthenticated ? <AccountPage /> : <Navigate to="/login" />} />
+        <Route path="/login" element={isAuthenticated ? <Navigate to="/account" /> : <LoginPage />} />
+        <Route path="/registration" element={isAuthenticated ? <Navigate to="/account" /> : <RegistrationPage />} />
       </Routes>
     </Router>
   );
