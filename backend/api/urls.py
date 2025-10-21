94711483de067f59302fd2683e49f498e0ea03d6
Бычков Евгений <ardrass@mail.ru>
2025-10-22 01:02:01 +0300

    add urls for update, create and delete news. Also added for get role

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,9 +5,13 @@ urlpatterns = [
     path("registration/", views.register_user, name="register_user"),
     path("login/", views.login_user, name="login_user"),
     path("news/", views.news_view, name="news"),
+    path("news/create/", views.create_news, name="create_news"),
+    path("news/<int:news_id>/", views.update_news, name="update_news"),
+    path("news/<int:news_id>/delete/", views.delete_news, name="delete_news"),
     path("documentation/", views.documentation_view, name="documentation"),
     path("download/", views.download_view, name="download"),
     path("profile/", views.get_user_profile, name="get_user_profile"),
     path("profile/update/", views.update_user_profile, name="update_user_profile"),
+    path("user/role/", views.check_user_role, name="check_user_role"),
     path("QA/", views.QA_view, name="QA"),
 ]
