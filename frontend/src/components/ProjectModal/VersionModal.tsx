38d253958a52d3ab24a1fee824b9f9dfcde695bc
Tim <timon.skvor@mail,ru>
2025-11-03 21:18:42 +0300

    Added window for view versions

---
+++ frontend/src/components/ProjectModal/VersionModal.tsx
@@ -0,0 +1,74 @@
+import React, {useEffect, useState} from "react";
+import "./VersionModal.scss";
+
+export interface VersionData {
+    id: number;
+    project_name: string;
+    description: string;
+    changer: string;
+}
+
+interface VersionModalProps {
+    projectId: number;
+    onClose: () => void;
+}
+
+const VersionModal: React.FC<VersionModalProps> = ({projectId, onClose}) => {
+    const [versions, setVersions] = useState<VersionData[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [projectName, setProjectName] = useState("");
+
+    useEffect(() => {
+        const fetchVersions = async () => {
+            try {
+                const response = await fetch(
+                    `http://localhost:8000/api/project/get_project_versions/${projectId}/`
+                );
+                if (!response.ok) console.error("Произошла ошибка.");
+                const data = await response.json();
+                if (data.length > 0) {
+                    setProjectName(data[0].project_name);
+                }
+                // Разворачиваем массив, чтобы последнее изменение было первым
+                setVersions(data);
+            } catch (err) {
+                console.error(err);
+                alert("Не удалось загрузить версии проекта");
+            } finally {
+                setLoading(false);
+            }
+        };
+        void fetchVersions();
+    }, [projectId]);
+
+    return (
+        <div className="version-modal">
+            <div className="version-modal-content">
+                <div className="version-modal-header">
+                    <h3>{"Версии проекта " + projectName}</h3>
+                    <button className="btn-close" onClick={onClose}>×</button>
+                </div>
+
+                {loading ? (
+                    <p>Загрузка...</p>
+                ) : versions.length === 0 ? (
+                    <p>Версий пока нет</p>
+                ) : (
+                    <div className="version-list">
+                        {versions.map((v, index) => (
+                            <div key={v.id} className="version-card">
+                                <div className="version-info">
+                                    <p><b>#{versions.length - index}</b></p>
+                                    <p><b>Автор изменения:</b> {v.changer}</p>
+                                    <p><b>Описание:</b> {v.description || "Без описания"}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default VersionModal;
